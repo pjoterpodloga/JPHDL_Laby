dcc7051de28575407cd69ccc5a7d36d3417ce92b
pjoterpodloga <106988465+pjoterpodloga@users.noreply.github.com>
2024-03-11 12:29:02 +0100

    Update div.v
    Dzialajacy dzielnik XDDDDDD.

--- RS232/RS232.srcs/sources_1/new/div.v
+++ RS232/RS232.srcs/sources_1/new/div.v
@@ -1,59 +1,28 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02.03.2024 17:47:59
-// Design Name: 
-// Module Name: div
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module div #(parameter NDIV = 10_417) (
+    input clk_i,
+    input rst_i,
+    output reg clk_o
+    );
 
-module div
-#( NDIV = 1)
-( input clk_i, input rst_i, output reg clk_o);
-    
-    integer counter = 0;
-    
-    initial
-    begin
-        clk_o = 1;
+integer counter = 0;
+
+always @(posedge clk_i or posedge rst_i) begin
+    if(rst_i) begin
+        counter <= 0;
+        clk_o <= 0;
     end
-    
-    always @ (posedge clk_i or posedge rst_i)
-    begin
-    
-        if (rst_i)
-            counter = 0;
-        else
-            counter = counter + 1;
-
-    end
-    
-    always @ (*)
-    begin
-    
-        if (counter == NDIV/2)
-            clk_o = 0;
-        else if (counter == NDIV)
-        begin
-            clk_o = 1;
-            counter = 0;
+    else if(clk_i) begin
+        counter <= counter + 1;
+        if(counter == NDIV / 2) begin
+            clk_o <= 1;
         end
-        else if (rst_i)
-            clk_o = 1;
-    
+        else if(counter == NDIV - 1) begin
+            counter <= 0;
+            clk_o <= 0;
+        end
     end
-
+end
+    
 endmodule
